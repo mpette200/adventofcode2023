00c3bac75c0eb7778d54b0871038588a9c772c36
mark.petterson <mpette200@gmail.com>
2023-12-07 01:00:19 +0000

    day 5a

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 mod day_blank;
 
 pub use day01::run as run_day01;
 pub use day02::run as run_day02;
 pub use day03::run as run_day03;
 pub use day04::run as run_day04;
+pub use day05::run as run_day05;
 pub use day_blank::run as run_day_blank;
