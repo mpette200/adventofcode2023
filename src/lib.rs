9ff57a48e0f4a76a28fba0773cc1e58179be8174
mark.petterson <mpette200@gmail.com>
2023-12-09 01:01:40 +0000

    day 8a

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod day04;
 mod day05;
 mod day06;
 mod day07;
+mod day08;
 mod day_blank;
 
 pub use day01::run as run_day01;
@@ -14,4 +15,5 @@ pub use day04::run as run_day04;
 pub use day05::run as run_day05;
 pub use day06::run as run_day06;
 pub use day07::run as run_day07;
+pub use day08::run as run_day08;
 pub use day_blank::run as run_day_blank;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use adventofcode2023 as lib;
 
 fn main() {
-    lib::run_day07();
+    lib::run_day08();
 }
