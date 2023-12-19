cb16e86989e5bf56bf47d1889045c1d788c7e219
mark.petterson <mpette200@gmail.com>
2023-12-19 20:36:23 +0000

    day 16a single thread

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ mod day11;
 mod day13;
 mod day14;
 mod day15;
+mod day16;
 mod day_blank;
 
 pub use day01::run as run_day01;
@@ -28,4 +29,5 @@ pub use day11::run as run_day11;
 pub use day13::run as run_day13;
 pub use day14::run as run_day14;
 pub use day15::run as run_day15;
+pub use day16::run as run_day16;
 pub use day_blank::run as run_day_blank;
